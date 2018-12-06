004fcce7d094b8ae3589217504a490dc17dbc6f2
nathan-castlehow <nathan_castlehow@me.com>
2018-12-06 13:28:51 +0800

    Started adding support for area checks

--- 2018/Day3/day3_p1.py
+++ 2018/Day3/day3_p1.py
@@ -8,24 +8,26 @@ def main():
 def _calculate_overlapped_claims(inputValues):
     claims_values_expression = "#(\d*) @ (\d*),(\d*): (\d*)x(\d*)"
     claims_values_regex = re.compile(claims_values_expression)
+    encountered_overlaps = set()
 
-    for claim in inputValues:
-        current_match = claims_values_regex.match(claim)
-        claim_id = current_match.group(1)
+    for i in range(len(inputValues)):
+        for j in (i,len(inputValues)):
+            current_match = claims_values_regex.match(claim)
+            claim_id = current_match.group(1)
 
-        #distances
-        left_dist = current_match.group(2)
-        top_dist = current_match.group(3)
+            #distances
+            left_dist = current_match.group(2)
+            top_dist = current_match.group(3)
 
-        #dimensions
-        width = current_match.group(4)
-        height = current_match.group(5)
+            #dimensions
+            width = current_match.group(4)
+            height = current_match.group(5)
 
-        print("Id:",claim_id)
-        print("LeftDist:", left_dist)
-        print("TopDist:",top_dist)
-        print("Width:",width)
-        print("Height:",height)
+            print("Id:",claim_id)
+            print("LeftDist:", left_dist)
+            print("TopDist:",top_dist)
+            print("Width:",width)
+            print("Height:",height)
         
 def _read_input_from_file():
     with open('example_input.txt') as f:
