0183c804705bd6fd7171a78ad7695b9840c16d12
Nathan Castlehow <nathan_castlehow@me.com>
2021-12-03 14:28:11 +0800

    Add day 3 part 1

---
+++ 2021/Day3/main.py
@@ -0,0 +1,39 @@
+import string
+
+from read_input import read_input_as_string
+import os
+
+
+def part_one():
+    abs_file_path = os.path.join(os.path.dirname(__file__), "input")
+    question_input = read_input_as_string(abs_file_path)
+
+    bit_positions_on = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    bit_positions_off = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    for i in range(0, len(question_input)):
+        line = question_input[i]
+        for j in range(0, len(question_input[i])):
+            char = line[j]
+            if char == "1":
+                bit_positions_on[j] += 1
+            else:
+                bit_positions_off[j] += 1
+
+    gamma_str = "".join(["0" if tup[0] > tup[1] else "1" for tup in zip(bit_positions_on, bit_positions_off)])
+    alpha_str = "".join(["1" if tup[0] > tup[1] else "0" for tup in zip(bit_positions_on, bit_positions_off)])
+
+    print("gamma: " + gamma_str)
+    print("alpha: " + alpha_str)
+
+    print("Total:" + str(int(gamma_str, 2) * int(alpha_str, 2)))
+
+
+def part_two():
+    abs_file_path = os.path.join(os.path.dirname(__file__), "input")
+    question_input = read_input_as_string(abs_file_path)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    part_one()
